a173ed810de4c7243a48b74f65abb97a64b2c709
chernushov88 <chernushov88@mail.ru>
2020-06-30 11:18:25 +0300

    javascript-interactive(lesson3) and new format writing lesson

--- JavascriptIsAdvanced/pages/01-DOM.js
+++ JavascriptIsAdvanced/pages/01-DOM.js
@@ -1,22 +1,23 @@
 document.title = "01-querySelector.js";
 let div = document.createElement('div');
-div.className = "lesson1";
-div.innerHTML = `<div id="app">id APP</div>
+div.className = 'lesson1';
+div.setAttribute("id", 'app');
+div.innerHTML = `<h1>H1 APP</h1>
     <div class="class-name">class-name</div>
     <ul>
         <li class="q">1</li>
         <li class="q">2</li>
         <li class="q">3</li>
         <li class="q">4</li>
-        <li class="q">5</li>
+        <li class="q">5</li> 
     </ul>
     <div id="test">
-        <h1>Hellow</h1>
+        <h2>Hellow</h2>
     </div>`;
 document.body.append(div)
 window.addEventListener('load', function () {
 
-    var id0 = document.getElementById('app');
+    let id0 = document.getElementById('app');
 
     var className0 = document.getElementsByClassName('q');
     console.log(id0);
@@ -24,9 +25,9 @@ window.addEventListener('load', function () {
 
 
     var id = document.querySelector('#app');
-    var className = document.querySelector('.q');
+    var className = document.querySelectorAll('.q');
     console.log(id);
     console.log(className);
-    var element = document.querySelector('#test  h1');
+    var element = document.querySelector('#test  h2');
     console.log(element);
 })
